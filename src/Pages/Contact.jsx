09327d6d648b603275db319b3ccf239a5c0dcea8
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-05-30 18:24:47 -0400

    added contact page

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <div>
+            <div>
+                <h3>Get in touch with me</h3>
+            </div>
+            <div>
+                <p>
+                    Currently looking for a new oppurtunitiy, feel free to contact me with the button below. I'll get back with you shortly.
+                </p>
+                <button>
+                    <a href="mailto:jaredunicycles@gmail.com">
+                        Contact Me
+                    </a>
+                </button>
+            </div>
+        </div>
+    );
+}
+export default Contact;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "./Header"
 import About from "./About"
+import Contact from "./Contact"
 
 const Home = () => {
     return (
         <>
            <Header id="Header" />
            <About id="About" />
+           <Contact />
         </>
     )
 }
