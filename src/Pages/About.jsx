fbd583dea3cec564a70cd23a686fc04c91105bc0
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-05-30 18:59:39 -0400

    setting up styling with tailwinds and postcss

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const About = () => {
     return (
-        <div>
+        <div id="About">
             <div>
-                <h2>
+                <h2 >
                 About Me
                 </h2>
             </div>
             <div>
-            <p>
+            <pre>
             Hello! My name is Jared Templeton. Looking for career change I found web development. 
             After enlisting in the Universty of Central Florida's Full Stack boot camp I knew this was the line of work I wanted to execute. 
             Now hoping for a chance to step into the development world and make some extraordinary responsive web apps. 
             While no where near an expert, I strive to learn and educate myself more everyday and hope to get started with a company soon.
-            </p>
+            </pre>
             <h4>Here are some technologies I currently work with: </h4>
             </div>
         </div>
--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Header () {
     return (
-        <div id="Header">
-            <div>
-                <p>Hi, my name is </p>
-                <h1>Jared Templeton</h1>
+        <div id="Header" className="mx-12 lg:mx-60 pt-60 items-center justify-center lg:w-1/2 text-justify">
+            <div className="flex flex-col">
+                <p className="text-lightblue_vs">Hi, my name is </p>
+                <h1 className="text-[#e6f1ff] text-7xl mt-5">Jared Templeton</h1>
             </div>
-            <button>
+            <button className="border border-lightblue_vs text-lightblue_vs mt-10 p-3 rounded hover:bg-opacity-10 hover:bg-lightblue_vs w-1/2">
             <a 
                     target="_blank"
                     rel="noreferrer"
