6b66108f0c792ce38ef46bf4db50c6808acf2b26
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-06-01 19:18:25 -0400

    fixed button stylign on header

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -4,16 +4,16 @@ function Header () {
     return (
         <div id="Header" className="mx-12 lg:mx-60 pt-60 items-center justify-center lg:w-1/2 text-justify">
             <div className="flex flex-col">
-                <p className="text-lightblue_vs">Hi, my name is </p>
+                <pre className="text-lightblue_vs">Hi, my name is, </pre>
                 <h1 className="text-[#e6f1ff] text-7xl mt-5">Jared Templeton</h1>
             </div>
-            <button className="border border-lightblue_vs text-lightblue_vs mt-10 p-3 rounded hover:bg-opacity-10 hover:bg-lightblue_vs w-1/2">
+            <button className="border border-lightblue_vs text-lightblue_vs mt-10 p-3 rounded hover:bg-opacity-10 hover:bg-lightblue_vs w-auto">
             <a 
                     target="_blank"
                     rel="noreferrer"
                     href="https://github.com/JaredTempleton"
                     alt="Link to my github">
-                Check out my Github
+                <pre>Check out my Github</pre>
             </a>
             </button>
         </div>
